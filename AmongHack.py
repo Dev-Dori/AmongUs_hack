5f2bf2104d968840b6319945cd6259f85cac6613
I-Dohyun-I <trouna43@gmail.com>
2021-02-18 01:38:31 +0900

    userinterface patch

--- AmongHack.py
+++ AmongHack.py
@@ -17,7 +17,7 @@ def ptraddr(main,ptrlist,mod):  # 포인터 주소 구하는 함수
     return addr + ptrlist[-1]
 
 def imposter_ch():
-    if int(pm.read_int(ptraddr(  ) == 1 :   # 메모리 주소 제거
+    if int(pm.read_int() == 1 :
         return 'O'
     else:
         return 'X'
@@ -31,79 +31,79 @@ try:   # 정상실행
     gameModule2 = module_from_name(pm.process_handle, "UnityPlayer.dll").lpBaseOfDll
     
     choice = 0
-    speed = pm.read_float(ptraddr(0x01C52828,[0x14, 0x38, 0x108, 0x64, 0x5C, 0x4, 0x14],1))
+    basespeed = pm.read_float()
+    speed = 0
     imposter_ch()
 
 
-
-    while choice != -1:
-        print("\n\n\t\t\t\t\=== 메뉴 ===/\n\n")
-        print("\t|\t [1] speed[ "+ str(speed) +" ] \t|\t [2] imposter[ "+ str(imposter_ch()) +" ] \t|\t [3] Kill Cooldown \t|\t [-1] 종료 ")
-        choice = int(input("요소를 선택하세요 : "))
-
-        if choice == -1: #선택 종료
-            break
-
-        elif choice == 1: # 스피드 변경
-            speed = float(input("속도를 입력하세요 : "))
-            pm.write_float(ptraddr(     )    # 메모리 주소 제거
-            print("\n 스피드 적용 성공")
-            time.sleep(1.5)
-            os.system('cls')
-
-        elif choice == 2: # 임포스터
-            if imposter_ch() == 'X':
-                pm.write_int(   ) # 메모리 주소 제거
-            else:
-                pm.write_int(   ) # 메모리 주소 제거
-            os.system('cls')
-        
-        elif choice == 3:
-            pm.write_float(   )   # 메모리 주소 제거
-            os.system('cls')
+    ### 스피드 설정 후 적용 ###
+    def apply():
+        if chkvar1.get() == 10: # 임포스터
+            pm.write_int())
+        else:
+            pm.write_int()
 
 
-        elif choice == 4:
-            pm.write_int(   )   # 메모리 주소 제거
+        if chkvar2.get() == 10: #노클립
+            pm.write_int(gameModule2+0x960C95,41605)
+        else:
+            pm.write_int(gameModule2+0x960C95,41604)
+
+
+        if chkvar3.get() == 10:
+            speed = w2.get()
+            pm.write_float(  ,float(speed))
+        else:
+            pm.write_float(  ,float(basespeed))
+
+
+    def killcool():
+        pm.write_float(  ,float(0))
+
+
+    ### 기본 설정 ###
+    win = Tk()
+    win.geometry("320x140")
+    win.title("어몽어스 핵")
+    win.minsize(width=320, height=140)
+    win.maxsize(width=320, height=140)
+
+    length_label = Label(win, text="Made By Dohyun (unifox)", bg="orange",width=70).place(x=0,y=0)
+
+    ### 체크리스트 ###
+    chkvar1 = IntVar()
+    chkvar2 = IntVar()
+    chkvar3 = IntVar()
+    chkbox1 = Checkbutton(win,text="임포스터", variable=chkvar1, onvalue=10, offvalue=20).place(x=10,y=25)#grid(row=0, column=0,padx = 1,pady = 0,ipadx=0)
+    chkbox2 = Checkbutton(win,text="No clip(벽뚫)", variable=chkvar2, onvalue=10, offvalue=20).place(x=110,y=25)
+
+
+    ### 이동속도 ###
+    chkbox3 = Checkbutton(win,text="스피드", variable=chkvar3,onvalue=10, offvalue=20).place(x=10,y=62)
+    w2 = Scale(win, from_=0, to=50, tickinterval= 50, orient=HORIZONTAL, length=230)
+    w2.set(10)
+    w2.place(x=75,y=45)
+
+    ### 적용과 킬쿨 ###
+    btn1 = Button(win, text='킬쿨 0초', command=killcool).place(x=10,y=110)
+    btn2 = Button(win, text='적용', command=apply, width=32).place(x=80,y=110)
+
+    win.mainloop()
 
 
 
 except: # 오류발생
     
-    ### 스피드 설정 후 적용 ###
-    def speedset():
-        print(w2.get())
-
-
     ### 기본 설정 ###
     win = Tk()
-    win.geometry("320x150")
+    win.geometry("320x140")
     win.title("어몽어스 핵")
-
-    ### 체크리스트 ###
-    length_label = Label(win, text="Made By Dohyun(unifox)").grid(row=0, column=1)
-    chkvar1 = IntVar()
-    chkvar2 = IntVar()
-    chkvar3 = IntVar()
-    chkbox1 = Checkbutton(win,text="임포스터", variable=chkvar1).grid(row=1, column=0,padx = 1,pady = 0,ipadx=0)
-    chkbox3 = Checkbutton(win,text="No clip(벽뚫)", variable=chkvar3).grid(row=1, column=1)
-
-
-    ### 이동속도 ###
-    chkbox2 = Checkbutton(win,text="스피드", variable=chkvar2).grid(row=2, column=0,ipadx=0)
-    w2 = Scale(win, from_=0, to=50, tickinterval= 50, orient=HORIZONTAL, length=230)
-    w2.set(10)
-    w2.grid(row=2, column=1)
-
-
-    btn1 = Button(win, text='킬쿨 0초', command=speedset).grid(row=4,column=0)
-    btn2 = Button(win, text='적용', command=speedset, width=30).grid(row=4,column=1)
-
-
-    
-    
-
-
+    win.minsize(width=320, height=140)
+    win.maxsize(width=320, height=140)
+    length_label = Label(win, text="Made By Dohyun (unifox)", bg="orange",width=70).place(x=0,y=0)
+    length_label2 = Label(win, text="오류발생!!",bg="yellow",foreground="red").place(x=130,y=24)
+    length_label3 = Label(win, text="프로그램 오류가 발생했습니다!\n게임에 들어간 이후\n프로그램을 다시 시작해주세요!").place(x=70,y=50)
+    length_label = Label(win, text="contact | trouna43@devdori.com", bg="orange",width=45).place(x=0,y=120)
     win.mainloop()
 
         
